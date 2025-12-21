1f54164f10fae5433b9cd4192a1784501925c1a6
TaniaAlex13 <ta.toapantac@uea.edu.ec>
2025-12-21 10:25:34 -0500

    clase de cliente

--- EjemplosMundoReal_POO/tienda_con_carrito.py
+++ EjemplosMundoReal_POO/tienda_con_carrito.py
@@ -5,3 +5,23 @@ class Producto:
         self.nombre = nombre
         self.precio = precio
 
+# Clase Producto
+class Producto:
+    def __init__(self, nombre, precio):
+        self.nombre = nombre
+        self.precio = precio
+
+
+# Clase Cliente
+class Cliente:
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.carrito = []
+
+    def agregar_al_carrito(self, producto):
+        self.carrito.append(producto)
+
+    def mostrar_carrito(self):
+        print("Carrito de", self.nombre)
+        for p in self.carrito:
+            print("-", p.nombre, "$", p.precio)
