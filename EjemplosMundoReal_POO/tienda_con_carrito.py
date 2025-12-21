a7515ef7c5e54057a7083dec2ae1d336082b08cb
TaniaAlex13 <ta.toapantac@uea.edu.ec>
2025-12-21 10:24:29 -0500

    clase de producto

---
+++ EjemplosMundoReal_POO/tienda_con_carrito.py
@@ -0,0 +1,7 @@
+#El cliente agrega productos a un carrito de compras
+# Clase Producto
+class Producto:
+    def __init__(self, nombre, precio):
+        self.nombre = nombre
+        self.precio = precio
+
