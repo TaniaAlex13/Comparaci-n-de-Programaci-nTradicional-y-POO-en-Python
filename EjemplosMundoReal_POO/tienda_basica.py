a8489f21490ed6f22e1fcce5245486198b38efc1
TaniaAlex13 <ta.toapantac@uea.edu.ec>
2025-12-21 09:56:28 -0500

    clase de cliente

--- EjemplosMundoReal_POO/tienda_basica.py
+++ EjemplosMundoReal_POO/tienda_basica.py
@@ -4,4 +4,11 @@ class Producto:
     def __init__(self, nombre, precio):
         self.nombre = nombre
         self.precio = precio
+# Clase Cliente
+# Representa al cliente de la tienda
+class Cliente:
+    def __init__(self, nombre):
+        self.nombre = nombre
 
+    def comprar(self, producto):
+        print(self.nombre, "compró", producto.nombre, "por $", producto.precio)
